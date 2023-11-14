84c2cc8623287513bdf392f44feed9e7aac38e3a
Hanz <2409281700@qq.com>
2023-11-14 17:04:21 +0800

    添加了beq,jal指令，添加了代码块（无跳转），循环块（beq）新增了用于构造标签的构造函数和方法，支持单独标签构造和在已有指令前添加标签

--- data_maker/src/Instr.java
+++ data_maker/src/Instr.java
@@ -11,13 +11,16 @@ import java.util.Random;
 public class Instr {
     public static int test = Main.test;
     public static int reg_start = 8, reg_end = 25, imm16_max = 65536, imm26_max = 0x4000000;
-    public static ArrayList<String> instrList = new ArrayList<>(Arrays.asList("add", "sub", "sw",
-            "lw", "ori", "lui"));
+    public static ArrayList<String> normList = new ArrayList<>(Arrays.asList("add", "sub", "sw",
+            "lw", "ori", "lui", "nop"));
     public static ArrayList<String> jumpList = new ArrayList<>(Arrays.asList("beq", "jal", "jr"));
+    public static int label_cnt = 0;
     public String randName;
     public int rand_rs, rand_rt, rand_rd, rand_imm16, rand_imm26;
     public String instrName;
     public int rs, rt, rd, imm16, imm26;
+    public ArrayList<String> labelList = new ArrayList<>();
+    public String rand_label, label;
 
     public Instr() {
         randInit();
@@ -25,12 +28,14 @@ public class Instr {
     }
 
     public Instr(String name) {
+        Main.myAssert(normList.contains(name), "normList.contains(name)");
         randInit();
         randName = name;
         init();
     }
 
     public Instr(String name, int n1) {
+        Main.myAssert(normList.contains(name), "normList.contains(name)");
         randInit();
         randName = name;
         setP1(n1);
@@ -38,6 +43,7 @@ public class Instr {
     }
 
     public Instr(String name, int n1, int n2) {
+        Main.myAssert(normList.contains(name), "normList.contains(name)");
         randInit();
         randName = name;
         setP1(n1);
@@ -46,6 +52,7 @@ public class Instr {
     }
 
     public Instr(String name, int n1, int n2, int n3) {
+        Main.myAssert(normList.contains(name), "normList.contains(name)");
         randInit();
         randName = name;
         setP1(n1);
@@ -75,20 +82,47 @@ public class Instr {
         init();
     }
 
-    public Instr(String[] names) {
-        this(names[new Random().nextInt(names.length)]);
+    public Instr(String name, String alabel) {
+        Main.myAssert(jumpList.contains(name) || name.equals("label"), "jumpList.contains(name)");
+        randInit();
+        randName = name;
+        rand_label = alabel;
+        init();
     }
 
-    public Instr(String[] names, int n1) {
-        this(names[new Random().nextInt(names.length)], n1);
+    public Instr(String name, int n1, String alabel) {
+        Main.myAssert(jumpList.contains(name), "jumpList.contains(name)");
+        randInit();
+        randName = name;
+        rand_label = alabel;
+        setP1(n1);
+        init();
     }
 
-    public Instr(String[] names, int n1, int n2) {
-        this(names[new Random().nextInt(names.length)], n1, n2);
+    public Instr(String name, int n1, int n2, String alabel) {
+        Main.myAssert(jumpList.contains(name), "jumpList.contains(name)");
+        randInit();
+        randName = name;
+        rand_label = alabel;
+        setP1(n1);
+        setP2(n2);
+        init();
     }
 
-    public Instr(String[] names, int n1, int n2, int n3) {
-        this(names[new Random().nextInt(names.length)], n1, n2, n3);
+    public Instr(ArrayList<String> names) {
+        this(names.get(new Random().nextInt(names.size())));
+    }
+
+    public Instr(ArrayList<String> names, int n1) {
+        this(names.get(new Random().nextInt(names.size())), n1);
+    }
+
+    public Instr(ArrayList<String> names, int n1, int n2) {
+        this(names.get(new Random().nextInt(names.size())), n1, n2);
+    }
+
+    public Instr(ArrayList<String> names, int n1, int n2, int n3) {
+        this(names.get(new Random().nextInt(names.size())), n1, n2, n3);
     }
 
     public void setP1(int n) {
@@ -152,14 +186,14 @@ public class Instr {
 
     public void randInit() {
         Random random = new Random();
-        int index = random.nextInt(instrList.size());
-        randName = instrList.get(index);
+        int index = random.nextInt(normList.size());
+        randName = normList.get(index);
         rand_rs = random.nextInt(reg_end - reg_start + 1) + reg_start;
         rand_rt = random.nextInt(reg_end - reg_start + 1) + reg_start;
         rand_rd = random.nextInt(reg_end - reg_start + 1) + reg_start;
         rand_imm16 = random.nextInt(imm16_max);
         rand_imm26 = random.nextInt(imm26_max);
-
+        rand_label = "";
     }
 
     public static int fix_regNum(int regNum) {
@@ -177,6 +211,7 @@ public class Instr {
         rd = fix_regNum(rand_rd);
         imm16 = rand_imm16;
         imm26 = rand_imm26;
+        label = rand_label;
 
         switch (instrName) {
             case "sw":
@@ -190,6 +225,13 @@ public class Instr {
 
 
     public void printInstr() {
+        if (instrName.equals("label")) {
+            System.out.println(label + ": ");
+            return;
+        }
+        for (String label : labelList) {
+            System.out.println(label + ": ");
+        }
         System.out.print(instrName + " ");
         switch (instrName) {
             case "add":
@@ -207,13 +249,13 @@ public class Instr {
                 System.out.println("$" + rt + " " + imm16 + "($" + rs + ")");
                 break;
             case "beq":
-                System.out.println("$" + rs + " $" + rt + " " + imm16);
+                System.out.println("$" + rs + " $" + rt + " " + label);
                 break;
             case "jr":
                 System.out.println("$" + rs);
                 break;
             case "jal":
-                System.out.println(imm26);
+                System.out.println(label);
                 break;
             case "nop":
                 System.out.println();
@@ -221,7 +263,7 @@ public class Instr {
     }
 
     public static void set_reg_range(int regMin, int regMax) {
-        Main.myAssert(regMin >= 0, "regMin >= 0");
+        Main.myAssert(regMin > 1, "regMin > 1");
         Main.myAssert(regMax <= 31, "regMax <= 31");
         reg_start = regMin;
         reg_end = regMax;
@@ -235,5 +277,11 @@ public class Instr {
     public static void set_imm26_range(int imm26MAx) {
         Main.myAssert(imm26MAx <= 0x4000000, "imm26MAx <= 0x4000000");
         imm26_max = imm26MAx;
+    }
+
+    public void set_label(String alabel) {
+        if (!labelList.contains(alabel)) {
+            labelList.add(alabel);
+        }
     }
 }
--- data_maker/src/Main.java
+++ data_maker/src/Main.java
@@ -3,10 +3,14 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static int test = 0;
+    public static int MAXINSTRNUM = 5000;
     public static ArrayList<Instr> instrList = new ArrayList<>();
+
     public static void Out() {
         try {
             PrintStream print = new PrintStream("test.txt");  //写好输出位置文件；
@@ -26,18 +30,49 @@ public class Main {
 
     public static void main(String[] args) {
         Out();
+//        block0(100, 8, 25);
 
-        for (int i = 0; i < 500; i++) {
-            Instr instr = new Instr(new String[]{"lw", "sw"});
-            instrList.add(instr);
-        }
-
+        beq_loop(10, 10, 4, 8, 25, "loop1");
+        beq_loop(100, 10, 4, 8, 25, "loop2");
+        beq_loop(10, 100, 4, 8, 25, "loop3");
         for (Instr instr : instrList) {
             instr.printInstr();
         }
     }
 
-    public void fwd_EtoD() {
+    public static void beq_loop(int cycles, int numPreCycle, int loop_reg, int reg_start,
+                                int reg_end, String alabel, String... anames) {
+        Instr.set_reg_range(reg_start, reg_end);
+        myAssert(loop_reg < reg_start || loop_reg > reg_end, "dead loop");
+
+        instrList.add(new Instr("ori", loop_reg, 0, 0)); // loop_reg = 0
+        instrList.add(new Instr("label", alabel + "_start")); // start label
+
+        block0(numPreCycle, reg_start, reg_end, anames); // 代码块
+
+        instrList.add(new Instr("ori", 1, 0, 1)); // $1 = 1
+        instrList.add(new Instr("add", loop_reg, loop_reg, 1)); // loop_reg += 1
+        instrList.add(new Instr("ori", 1, 0, cycles)); // $1 = cycles
+        instrList.add(new Instr("beq", 1, loop_reg, alabel + "_end")); // if ($1 == loop_Reg) than end
+        instrList.add(new Instr()); // none jump
+        instrList.add(new Instr("jal", alabel + "_start")); // jal start
+        instrList.add(new Instr()); // none jump
+        instrList.add(new Instr("label", alabel + "_end"));// end label
+
         return;
+    }
+
+    public static void block0(int num, int reg_start, int reg_end, String... anames) {
+        Instr.set_reg_range(reg_start, reg_end);
+        ArrayList<String> names;
+        if (anames.length == 0) {
+            names = Instr.normList;
+        } else {
+            names = new ArrayList<>(Arrays.asList(anames));
+        }
+
+        for (int i = 0; i < num; i++) {
+            instrList.add(new Instr(names));
+        }
     }
 }
